a3d374ee254d25fb340f8588e61ec0c43dbe5e6e
Elvin Valentino <elvin.valentino@gmail.com>
2025-06-27 11:32:53 +0700

    add middleware

--- main.py
+++ main.py
@@ -2,14 +2,28 @@ from typing import Union
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
 import scraper
 
 app = FastAPI()
 
+origins = [
+    "https://a.hicomsystem.com",
+    "http://a.hicomsystem.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World", "version": "1.0.1"}
 
 class Scrape(BaseModel):
     url: str
